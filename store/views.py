90c4d7814874cca0f7cd6bcfee97eec39a529d96
kendrickPC <kennethpchang@gmail.com>
2023-06-07 12:29:00 +0800

    rest_framework.viewsets import ModelViewSet

--- store/views.py
+++ store/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.viewset import ModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
