f240473935c9cd203290ebac42828de5b6a1f531
kendrickPC <kennethpchang@gmail.com>
2023-06-07 13:29:47 +0800

    Reviews model migrated

--- store/models.py
+++ store/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 
+class Review(models.Model):
+    product = models.ForeignKey(
+        'Product', on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
